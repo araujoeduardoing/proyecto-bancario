126cd5c824afe41b14d7b63e39f18d5bdb05c0af
EDUARDO ALFONSO ARAUJO ESCOBAR <earaujoe@pichincha.com>
2026-03-01 16:46:56 -0500

    inicio backend

---
+++ backend/liq-msa-bp-customer/src/main/java/liq_msa_bp_customer/LiqMsaBpCustomerApplication.java
@@ -0,0 +1,13 @@
+package liq_msa_bp_customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiqMsaBpCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiqMsaBpCustomerApplication.class, args);
+	}
+
+}
---
+++ backend/liq-msa-bp-customer/src/test/java/liq_msa_bp_customer/LiqMsaBpCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package liq_msa_bp_customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiqMsaBpCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
