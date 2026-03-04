7eefa0671552c0d5b7270d01ec6e6fd9c921950e
EDUARDO ALFONSO ARAUJO ESCOBAR <earaujoe@pichincha.com>
2026-03-03 21:03:51 -0500

    Creacion de front cuentas y movimientos Inicio

---
+++ frontend/movimientos-mf/webpack.config.js
@@ -0,0 +1,15 @@
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+
+  name: 'movimientos-mf',
+
+  exposes: {
+    './Component': './src\app\app.ts',
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+});
